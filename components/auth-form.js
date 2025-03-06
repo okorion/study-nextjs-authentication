334b35e5ff5f2b0143979a8e83a2692918d27485
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-03-07 01:02:20 +0900

    feat: 로그인 시 이메일, 패스워드 유효성 검사 및 에러 처리

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,6 +1,12 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+
+import { signup } from "@/actions/auth-actions";
 
 export default function AuthForm() {
+  const [formState, formAction] = useFormState(signup, {});
+
   return (
     <form id="auth-form">
       <div>
@@ -14,10 +20,15 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (
+        <ul id="form-errors">
+          {Objects.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
       <p>
-        <button type="submit">
-          Create Account
-        </button>
+        <button type="submit">Create Account</button>
       </p>
       <p>
         <Link href="/">Login with existing account.</Link>
