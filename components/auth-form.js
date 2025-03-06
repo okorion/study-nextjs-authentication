45be677b0fd3c0c608f85e0079d4e9358e75f730
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-03-07 01:53:22 +0900

    feat: searchParams 인증 모드 전환

--- components/auth-form.js
+++ components/auth-form.js
@@ -4,7 +4,7 @@ import { useFormState } from "react-dom";
 
 import { signup } from "@/actions/auth-actions";
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
   const [formState, formAction] = useFormState(signup, {});
 
   return (
@@ -27,11 +27,14 @@ export default function AuthForm() {
           ))}
         </ul>
       )}
+      <p>{mode === "login" ? "Login" : "Create Account"}</p>
       <p>
-        <button type="submit">Create Account</button>
-      </p>
-      <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
